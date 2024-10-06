af58908929e6c376fc3af8195c7a3e7cb8fe7df8
Angel <angel.santiago@tutanota.com>
2024-10-05 21:52:52 -0400

    Remove explicit return typing lint rule

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,3 +1,5 @@
+// const { rule } = require('postcss')
+
 module.exports = {
   extends: [
     'eslint:recommended',
@@ -5,5 +7,8 @@ module.exports = {
     'plugin:react/jsx-runtime',
     '@electron-toolkit/eslint-config-ts/recommended',
     '@electron-toolkit/eslint-config-prettier'
-  ]
+  ],
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off'
+  }
 }
