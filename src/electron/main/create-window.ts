21dfc60a494cdf049e78df526d13bff966fe28a5
Angel <angel.santiago@tutanota.com>
2024-10-10 21:44:13 -0400

    Code cleanup

--- src/electron/main/create-window.ts
+++ src/electron/main/create-window.ts
@@ -1,7 +1,7 @@
-import { shell, BrowserWindow } from 'electron'
-import { join } from 'node:path'
-import { is } from '@electron-toolkit/utils'
-import icon from '../../../resources/icon.png?asset'
+import { shell, BrowserWindow } from 'electron';
+import { join } from 'node:path';
+import { is } from '@electron-toolkit/utils';
+import icon from '../../../resources/icon.png?asset';
 
 export function createWindow(): void {
   const mainWindow = new BrowserWindow({
@@ -18,22 +18,22 @@ export function createWindow(): void {
       preload: join(__dirname, '../preload/index.js'),
       sandbox: false
     }
-  })
+  });
 
   mainWindow.on('ready-to-show', () => {
-    mainWindow.show()
-  })
+    mainWindow.show();
+  });
 
   mainWindow.webContents.setWindowOpenHandler((details) => {
-    shell.openExternal(details.url)
-    return { action: 'deny' }
-  })
+    shell.openExternal(details.url);
+    return { action: 'deny' };
+  });
 
   // HMR for renderer base on electron-vite cli.
   // Load the remote URL for development or the local html file for production.
   if (is.dev && process.env.ELECTRON_RENDERER_URL) {
-    mainWindow.loadURL(process.env.ELECTRON_RENDERER_URL)
+    mainWindow.loadURL(process.env.ELECTRON_RENDERER_URL);
   } else {
-    mainWindow.loadFile(join(__dirname, '../renderer/index.html'))
+    mainWindow.loadFile(join(__dirname, '../renderer/index.html'));
   }
 }
