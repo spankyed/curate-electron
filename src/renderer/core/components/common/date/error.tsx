73ab8db0059989b161c2685e342dfca51ec0e427
Angel <angel.santiago@tutanota.com>
2025-04-07 16:35:20 -0400

    Update copy

--- src/renderer/core/components/common/date/error.tsx
+++ src/renderer/core/components/common/date/error.tsx
@@ -3,9 +3,12 @@ import { Typography } from '@mui/material';
 
 const ErrorState = () => {
   return (
-    <div className="flex flex-col items-center p-6 max-w-md mx-auto my-8">
-      <Typography variant="h3" sx={{ opacity: 0.3, color: 'red' }}>
+    <div className="flex flex-col items-center p-4 mx-auto my-8">
+      <Typography variant="h4" sx={{ opacity: 0.2, color: 'red' }}>
         Papers Unavailable
+      </Typography>
+      <Typography variant="body1" sx={{ opacity: 0.3, color: 'red' }}>
+        Please try again later.
       </Typography>
     </div>
   );
